cadbc53ff47fdba4f264dd8a19118053fa849fd1
caseyvien <kesnoveno@gmail.com>
2017-09-06 22:52:39 +0800

    added Person

---
+++ classes/doctor.py
@@ -0,0 +1,11 @@
+from .person import Person
+
+class Doctor(Person):
+
+    def __init__(self, d_name, specialties):
+        Person.__init__(self,"Doctor",d_name)
+        self.specialties = specialties
+
+    def display_doctor(self):
+        print("Doctor\'s name:", self.p_name)
+        print("Specialties:", self.specialties)
